7c4ba422773f3b8e7bfe4aa344052e156bf36508
Dmitriy Savchenko <login.was.here@gmail.com>
2013-03-28 01:46:22 +0200

    Change file search to use actors

--- src/clojure_course_task02/core.clj
+++ src/clojure_course_task02/core.clj
@@ -2,14 +2,29 @@
   (:import java.io.File)
   (:gen-class))
 
+(def files
+    (agent []))
+
+(defn addFile [files file]
+    (vec (conj files file)))
+
+(defn sendFile [files file]
+    (send files addFile file))
+
 (defn find-files [file-name path]
   "TODO: Implement searching for a file using his name as a regexp."
-  (filter
-    #(not
-      (empty?
-        (re-find
-          (re-pattern file-name) %)))
-      (map #(. % getName) (file-seq (clojure.java.io/file path)))))
+  (doall
+    (pmap
+      (fn [file]
+        (future
+          (if
+            (not
+              (empty?
+                (re-find
+                  (re-pattern file-name) (.getName file))))
+            (sendFile files (.getName file)))))
+      (file-seq (clojure.java.io/file path))))
+  (deref files))
 
 (defn usage []
   (println "Usage: $ run.sh file_name path"))
